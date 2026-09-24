a62f638e603142593b9ea8e43ae1729b0ecf0875
Влад Цветков <tsvetkov.vm@phystech.edu>
2026-09-24 11:06:10 +0300

    Добавлены два новых файла

---
+++ get-led/double-push.py
@@ -0,0 +1,40 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+led = 26
+GPIO.setup(led, GPIO.OUT)
+
+leds = [16, 12, 25, 17, 27, 23, 22, 24]
+GPIO.setup(leds, GPIO.OUT)
+GPIO.output(leds, 0)
+
+up = 9
+down = 10
+GPIO.setup(up, GPIO.IN)
+GPIO.setup(down, GPIO.IN)
+num = 0
+
+def dec2bin(value):
+    return[int(element) for element in bin(value)[2:].zfill(8)]
+sleep_time = 0.2
+
+
+while True:
+    if GPIO.input(up) and GPIO.input(down):
+        num = 255
+        print (num, dec2bin(num))
+        time.sleep(sleep_time)
+
+    elif GPIO.input(up):
+        if num < 255:
+            num = num+1
+            print (num, dec2bin(num))
+            time.sleep(sleep_time)
+
+    elif GPIO.input(down):
+        if num > 0:
+            num = num - 1
+            print (num, dec2bin(num))
+            time.sleep(sleep_time)
+    GPIO.output(leds, dec2bin(num))
